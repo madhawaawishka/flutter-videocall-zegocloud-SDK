3fb8222c203c33c208424f764aeedf0f82768dd1
madhawaawishka99 <madhawaawishka@gmail.com>
2024-10-03 23:50:05 +0530

    test changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Video Call Manager',
+      title: 'Video Call Managers',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
